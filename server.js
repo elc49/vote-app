e06b9715a9a9550acee6bbb4fbac4188659a941b
3dw1nM0535 <workockmoses@gmail.com>
2017-10-19 18:45:48 +0300

    added semantic package module for ui

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/config', express.static(process.cwd() + '/config'));
 app.use('/controllers', express.static(process.cwd() + '/controllers'));
+app.use('/semantic', express.static(process.cwd() + '/semantic'));
 
 //Body parser Middleware
 app.use(bodyParser.json());
