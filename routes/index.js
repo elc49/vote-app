59080d425dbabdb655727f4869fda1863878d192
3dw1nM0535 <workockmoses@gmail.com>
2017-10-19 21:35:50 +0300

    config route for new poll

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ module.exports = function (app) {
   app.route('/').get(function (req, res) {
     res.render('index');
   });
+
+  //New vote route
+  app.route('/polls/newvote').get(function (req, res) {
+    res.render('form');
+  });
 };
