38c0ed0cc649bd0c47d2088b4bcac0adb91ca416
3dw1nM0535 <workockmoses@gmail.com>
2017-10-19 11:55:33 +0300

    configure routes

--- server.js
+++ server.js
@@ -2,9 +2,26 @@
 
 var express =require('express');
 var bodyParser = require('body-parser');
+var routes = require('./routes/index');
 
 //Instantiate express object
 var app = express();
+
+//Route Middleware
+routes(app);
+
+//View Middleware
+app.set('views', process.cwd() + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+//Static files Middleware
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/config', express.static(process.cwd() + '/config'));
+app.use('/controllers', express.static(process.cwd() + '/controllers'));
 
 //Body parser Middleware
 app.use(bodyParser.json());
