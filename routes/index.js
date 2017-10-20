4fae4553825091a542e746c74fd21391a279c083
3dw1nM0535 <workockmoses@gmail.com>
2017-10-20 04:09:59 +0300

    defined route handlers for user

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,48 @@
 //Routes handler
 
-var express= require('express');
+var votesHandler = require('../controllers/serverHandler');
 
-module.exports = function (app) {
+module.exports = function (app, passport) {
+
+  function IsLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      res.redirect('/login');
+    }
+  }
+
+  var voteHandler = new votesHandler();
+  
+  //Home route if req.isAuthenticated()
   app.route('/').get(function (req, res) {
-    res.render('index');
+    res.render('index', { user: req.user });
   });
+
+  //Login route if !req.isAuthenticated()
+  app.route('/login').get(function (req, res) {
+    res.render('/login', voteHandler.getVotes);
+  });
+
+  //Logout route handler
+  app.route('/logout').get(function (req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  //Profile route handler
+  app.route('/profile').get(IsLoggedIn, function (req, res) {
+    res.render('profile');
+  });
+
+  //Authenticate route
+  app.route('/auth/twitter').get(passport.authenticate('twitter'));
+
+  //Callback route if auth === true
+  app.route('/auth/twitter/callback').get(passport.authenticate('twitter', { 
+    successRedirect: '/',
+    failureRedirect: '/login'
+   }));
 
   //New vote route
   app.route('/polls/newvote').get(function (req, res) {
