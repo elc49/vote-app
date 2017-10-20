a37db5a5cb18f9ede069b3c1faa991eaad50ae35
3dw1nM0535 <workockmoses@gmail.com>
2017-10-20 04:09:29 +0300

    defined Oauth paramaters

--- config/auth.js
+++ config/auth.js
@@ -1,1 +1,9 @@
-g
+//auth config vars
+
+module.exports = {
+  'twitterAuth': {
+    'clientID': process.env.TWITTER_KEY,
+    'clientSecret': process.env.TWITTER_SECRET,
+    'callbackURI': process.env.APP_URI + '/auth/twitter/callback' 
+  }
+};
