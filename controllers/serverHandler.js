6a1903131fe70577dfaf5fa4c9532c92603782bd
3dw1nM0535 <workockmoses@gmail.com>
2017-10-23 03:13:15 +0300

    redefined extra whitespace

--- controllers/serverHandler.js
+++ controllers/serverHandler.js
@@ -1,20 +1,30 @@
 //Create various handlers for server interaction
 var User = require('../models/model');
 
-function votesHandler () {
+function votesHandler() {
 
-  this.getVotes = function (req, res) {
-    User.findOne({ 'twitter.id': req.user.twitter.id }, { '_id': false}).exec(function (err, data) {
+  this.getVotes = function(req, res) {
+    User.findOne({
+      'twitter.id': req.user.twitter.id
+    }, {
+      '_id': false
+    }).exec(function(err, data) {
       if (err) {
         throw err;
       }
-      
+
       console.log(data);
     });
   };
 
-  this.addVotes = function (req, res) {
-    User.findOneAndUpdate({ 'twitter.id': req.user.twitter.id }, { $push: { 'votes': req.body.vote } }).exec(function (err, data) {
+  this.addVotes = function(req, res) {
+    User.findOneAndUpdate({
+      'twitter.id': req.user.twitter.id
+    }, {
+      $push: {
+        'votes': req.body.vote
+      }
+    }).exec(function(err, data) {
       if (err) {
         throw err;
       }
@@ -25,4 +35,4 @@ function votesHandler () {
 
 }
 
-module.exports= votesHandler; 
+module.exports = votesHandler;
