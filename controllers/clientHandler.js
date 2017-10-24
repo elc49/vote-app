291661676d034312f3d833df663f194a08e2190b
3dw1nM0535 <workockmoses@gmail.com>
2017-10-24 23:12:23 +0300

    client handler Middleware

--- controllers/clientHandler.js
+++ controllers/clientHandler.js
@@ -4,10 +4,26 @@
   $('.lead').css({
     'font-size': '3rem',
     'font-family': 'Ubuntu',
-    'font-weight': 200
+    'font-weight': 400
   });
   $('.twitter').css({
     'font-size': '1.50rem'
+  });
+  $('.tagline').css({
+    'font-size': '2rem'
+  });
+  $('.marginable').css({
+    'font-size': '4rem',
+    'color': 'green',
+    'margin-top': '3rem'
+  });
+
+  $('.marginable').transition({
+    animation: 'jiggle',
+    duration: '1s',
+    onComplete: function () {
+      $(this).transition('fade');
+    }
   });
 
 })();
