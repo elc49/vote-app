09b8500fcb264c704cdcc8ef58a97da2cea6e180
3dw1nM0535 <workockmoses@gmail.com>
2017-10-24 17:59:41 +0300

    setup passport and console output for test

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,31 @@
 //Authentication setup
+
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
+var keys = require(process.cwd() + '/config/auth');
+var User = require(process.cwd() + '/models/model');
+
+//serializeUser()/deserializeUser() user Middleware
+/*
+passport.serializeUser(function (user, done) {
+  done(null, done);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id).then(function (user) {
+    done(null, user);
+  });
+});
+*/
+//Setup passport Middleware
+passport.use(
+  new TwitterStrategy ({
+    consumerKey: 'U9VEjGV6DhGLDzlYklvkLgE3j',
+    consumerSecret: 'AVjtxIh9x0p54QdjDAIIqcm41TudkYoyqvJsOneML4MR6299JP',
+    callbackURL: keys.twitter.callbackURL
+
+  }, function (token, tokenSecret, profile, done) {
+    console.log(profile);
+  })
+);
+
