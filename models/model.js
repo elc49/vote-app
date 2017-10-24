75b679b2836e94f215ffe8b2dfcd12d5a98bf4fe
3dw1nM0535 <workockmoses@gmail.com>
2017-10-25 01:04:04 +0300

    user data for img schema

--- models/model.js
+++ models/model.js
@@ -4,8 +4,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
+  img: {
+    data: Buffer,
+    contentType: String
+  },
   _id: String,
   username: String,
+  displayName: String,
   options: [String],
   votes: [String]
 }); 
