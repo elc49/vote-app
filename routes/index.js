867638b735b93f3cfa9422e3d34658805e74158f
3dw1nM0535 <workockmoses@gmail.com>
2017-10-24 14:35:00 +0300

    resovle var

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var router = require('express').Router();
 
-//Home route
+//Home route handler
 router.get('/', function (req, res) {
   res.send('Home route');
 });
+
+//new poll route handler
+router.get('/newPoll', function (req, res) {
+  res.render('newPoll');
+});
+
+//my polls route handler
+router.get('/myPolls', function (req, res) {
+  res.send('This is my polls route');
+});
+
 
 module.exports = router;
