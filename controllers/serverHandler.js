7098adc9c4b6d1d7f82384f2f36abb79a8d30685
3dw1nM0535 <workockmoses@gmail.com>
2017-10-25 19:58:01 +0300

    defining buffer decoder function method

---
+++ controllers/serverHandler.js
@@ -0,0 +1,13 @@
+//Encode() and decode() function definition
+
+function base64() {
+  this.encode = function (arrayBuffer) {
+    return new Buffer (arrayBuffer || '').toString('base64');
+  };
+
+  this.decode = function (arrayBase64) {
+    return new Buffer(arrayBase64 || '', base64).toString('utf8');
+  };
+}
+
+module.exports = base64;
