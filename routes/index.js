a2d93f2ebadefad39eb98208e1d017a30aff831f
3dw1nM0535 <workockmoses@gmail.com>
2017-10-25 22:23:41 +0300

    defined default user authenticated home route

--- routes/index.js
+++ routes/index.js
@@ -15,16 +15,15 @@ var ensureAuthenticated = function (req, res, next) {
 };
 
 //home route handler
-router.get('/', ensureAuthenticated, function (req, res) {
+router.get('/', function (req, res) {
 
   res.render('polls', {
     user: req.user,
-    img: base64.decode(req.user.img.data)
   });
 });
 
 //new poll route handler
-router.get('/newPoll', function (req, res) {
+router.get('/newPoll', ensureAuthenticated, function (req, res) {
 
   res.render('newPoll', {
     user: req.user,
@@ -33,7 +32,7 @@ router.get('/newPoll', function (req, res) {
 });
 
 //my polls route handler
-router.get('/myPoll', function (req, res) {
+router.get('/myPoll', ensureAuthenticated, function (req, res) {
 
   res.render('myPoll', {
     user: req.user,
