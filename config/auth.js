5d82467f4515b835c29a3b876c0e37060b8c9922
3dw1nM0535 <workockmoses@gmail.com>
2017-10-25 14:28:48 +0300

    ommited

--- config/auth.js
+++
@@ -1,9 +0,0 @@
-//Authentication keys
-
-module.exports = {
-  twitter: {
-    consumerKey: process.env.CONSUMER_KEY,
-    consumerSecret: process.env.CONSUMER_SECRET,
-    callbackURL: 'https://opinion-polling.herokuapp.com/auth/twitter/callback'
-  }
-};
