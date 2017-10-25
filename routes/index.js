20ce7c2493d375bdd2e00982aa9e5de59b4871fc
3dw1nM0535 <workockmoses@gmail.com>
2017-10-25 20:00:14 +0300

    user route defined

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,33 @@
 var router = require('express').Router();
+var decoder = require(process.cwd() + '/controllers/serverHandler');
+
+var base64 = new decoder();
 
 //home route handler
 router.get('/', function (req, res) {
-  res.render('home', { user: req.user });
+
+  res.render('home', { 
+    user: req.user,
+    img: base64.decode(req.user.img.data) 
+  });
 });
 
 //new poll route handler
 router.get('/newPoll', function (req, res) {
-  res.render('newPoll', { user: req.user });
+
+  res.render('newPoll', { 
+    user: req.user,
+    img: base64.decode(req.user.img.data) 
+  });
 });
 
 //my polls route handler
 router.get('/myPoll', function (req, res) {
-  res.render('myPoll', { user: req.user });
+
+  res.render('myPoll', { 
+    user: req.user,
+    img: base64.decode(req.user.img.data) 
+  });
 });
 
 
