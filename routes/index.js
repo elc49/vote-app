1982564abb43bf264e3a61a8935c3892aa6dd600
3dw1nM0535 <workockmoses@gmail.com>
2017-10-25 21:41:27 +0300

    redefined authenticated user

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,43 @@
 var router = require('express').Router();
 var decoder = require(process.cwd() + '/controllers/serverHandler');
+//var ensureAuthenticated = require(process.cwd() + '/controllers/serverHandler');
 
 var base64 = new decoder();
 
-//home route handler
-router.get('/', function (req, res) {
+//ensure user isAuthenticated() Middleware
 
-  res.render('home', { 
+var ensureAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+};
+
+//home route handler
+router.get('/', ensureAuthenticated, function (req, res) {
+
+  res.render('polls', {
     user: req.user,
-    img: base64.decode(req.user.img.data) 
+    img: base64.decode(req.user.img.data)
   });
 });
 
 //new poll route handler
 router.get('/newPoll', function (req, res) {
 
-  res.render('newPoll', { 
+  res.render('newPoll', {
     user: req.user,
-    img: base64.decode(req.user.img.data) 
+    img: base64.decode(req.user.img.data)
   });
 });
 
 //my polls route handler
 router.get('/myPoll', function (req, res) {
 
-  res.render('myPoll', { 
+  res.render('myPoll', {
     user: req.user,
-    img: base64.decode(req.user.img.data) 
+    img: base64.decode(req.user.img.data)
   });
 });
 
