5f43dcf12672d42ca5ac266d1bd184e68f6b2a10
3dw1nM0535 <workockmoses@gmail.com>
2017-10-25 13:24:31 +0300

    redefined callbackURL

--- config/auth.js
+++ config/auth.js
@@ -4,6 +4,6 @@ module.exports = {
   twitter: {
     consumerKey: process.env.CONSUMER_KEY,
     consumerSecret: process.env.CONSUMER_SECRET,
-    callbackURL: 'http://localhost:8080/auth/twitter/callback'
+    callbackURL: 'https://opinion-polling.herokuapp.com/auth/twitter/callback'
   }
 };
