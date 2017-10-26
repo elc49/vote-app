6cd03b41444802631e3d14c5a9017fa8977e73c4
3dw1nM0535 <workockmoses@gmail.com>
2017-10-26 16:10:35 +0300

    selector styling

--- controllers/clientHandler.js
+++ controllers/clientHandler.js
@@ -2,9 +2,9 @@
 
   $('.outer').css('border-radius', 0);
   $('.lead').css({
-    'font-size': '3rem',
+    'font-size': '3.5rem',
     'font-family': 'Ubuntu',
-    'font-weight': 400
+    'font-weight': 200
   });
   $('.twitter').css({
     'font-size': '1.50rem'
