cd877a0ad8d0134dbdfb5dba0826ba895d48298f
3dw1nM0535 <workockmoses@gmail.com>
2017-11-02 01:18:39 +0300

    added url to res.locals var

--- server.js
+++ server.js
@@ -72,6 +72,7 @@ app.use(function (req, res, next) {
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
   res.locals.user = req.user || null;
+  res.locals.url = req.get('host')+req.originalUrl;
   next();
 });
 
